78bffaaaec59df6c1912657cf204a6ac63fefff7
Daniel Rodriguez <MissingRed@github.com>
2021-01-18 20:50:15 -0500

    Cambio de contraseña en componente perfil

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../Components/Navbar";
 import Chip from "../Components/Chip";
 import Sidebar from "../Components/Sidebar";
@@ -6,10 +6,12 @@ import Swal from "sweetalert2";
 import "../Styles/Profile.css";
 
 import { AuthContext } from "../Database/Auth";
+import app from "../Database/Base.js";
 
 const Profile = () => {
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
+  const [password, setPassword] = useState(false);
+  //   console.log(currentUser);
 
   const buttonVerified = () => {
     currentUser
@@ -25,6 +27,40 @@ const Profile = () => {
         Swal.fire("Error", "Error inesperado", "error");
         console.log(error);
       });
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const { password, Newpassword } = event.target.elements;
+    try {
+      await app
+        .auth()
+        .signInWithEmailAndPassword(currentUser.email, password.value);
+      currentUser
+        .updatePassword(Newpassword.value)
+        .then(function () {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Contraseña cambiada con exito",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        })
+        .catch(function (error) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Algo salio mal!, Vuelve a intentarlo",
+          });
+        });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "La contraseña actual es incorrecta!",
+      });
+    }
   };
 
   return (
@@ -52,24 +88,23 @@ const Profile = () => {
                           <p className="negrilla">E-Mail</p>
                           <p>{currentUser.email}</p>
                         </div>
-                        <div className="wrapp">
-                          <p className="negrilla">Verificación de Email</p>
-                          <div>
-                            {currentUser.emailVerified === false ? (
-                              <button
-                                onClick={buttonVerified}
-                                className="main-profile__button"
-                              >
-                                <img src="Img/alert.svg" alt="Alert" />
-                                Enviar correo de verificación
-                              </button>
-                            ) : (
-                              <div className="verificado">
-                                <span>Correo Verifiado</span>
-                                <img src="Img/check.svg" alt="Check" />
-                              </div>
-                            )}
-                          </div>
+
+                        <p className="negrilla">Verificación de Email</p>
+                        <div>
+                          {currentUser.emailVerified === false ? (
+                            <button
+                              onClick={buttonVerified}
+                              className="main-profile__button"
+                            >
+                              <img src="Img/alert.svg" alt="Alert" />
+                              Enviar correo de verificación
+                            </button>
+                          ) : (
+                            <div className="verificado">
+                              <span>Correo Verifiado</span>
+                              <img src="Img/check.svg" alt="Check" />
+                            </div>
+                          )}
                         </div>
                       </div>
                       <div className="img">
@@ -81,18 +116,58 @@ const Profile = () => {
                           }
                           alt="User"
                         />
+                        <div className="info-edit">
+                          <p className="negrillaul">Ultima conexión</p>
+                          <p className="tiempo">
+                            {currentUser.metadata.lastSignInTime}
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="info-edit">
-                    <p className="negrilla">Ultima conexión</p>
-                    <p>{currentUser.metadata.lastSignInTime}</p>
                   </div>
                 </div>
               </div>
               <div className="change">
                 <div className="probar1">
-                  <p className="negrilla">Contraseña</p>
+                  <p className="negrillaPass">Contraseña</p>
+
+                  {password ? (
+                    <form
+                      className="main-log_container-form_form"
+                      onSubmit={handleLogin}
+                    >
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Contraseña actual"
+                        className="main-log_container-form_input"
+                      />
+                      <input
+                        type="password"
+                        name="Newpassword"
+                        placeholder="Nueva contraseña"
+                        className="main-log_container-form_input"
+                      />
+                      <button
+                        className="main-log__container-options_button"
+                        type="submit"
+                      >
+                        Guardar contraseña
+                      </button>
+                    </form>
+                  ) : (
+                    <div>
+                      <span>*******************</span>
+                      <div>
+                        <button
+                          className="main-profile__password_button"
+                          onClick={() => setPassword(!password)}
+                        >
+                          Modificar Contraseña
+                        </button>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
